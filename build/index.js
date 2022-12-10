c500d598671ecb957ea90ec4d262e6eda728f540
Hector Diaz | U-Payments <hector.diaz@u-payments.com>
2022-12-10 09:06:15 -0300

    first commit

---
+++ build/index.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Starting server with node and Typescript");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log("Starting server with node and Typescript");
+
+interface Medic {
+  name: string;
+  codMed: number;
+}
+
+const medic: Medic = { name: "Hector", codMed: 1 };
+
+console.log(medic);
